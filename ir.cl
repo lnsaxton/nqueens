0d6831e8ccd44df9da2e4dfffd76554413398b99
nlacock <nlacock@nlacock-desktop.(none)>
2012-05-13 13:58:48 -0400

    Implemented reduce and make move kernels, added pseudorandom tie-breaking, found some example seeds.

--- ir.cl
+++ ir.cl
@@ -13,6 +13,7 @@
   To avoid a jump in the internal loop, if an occupied square is checked then
     nqueens is added to its conflicts: this marks the square as occupied and
     will prevent it from being chosen as the min square
+
 */
 __kernel void count_conflicts(const __global queen * queens,
 			      __global queen * result,
@@ -22,11 +23,11 @@ __kernel void count_conflicts(const __global queen * queens,
   int q = get_group_id(0);
   int col = get_local_id(0);
 
-  /*
+  
   event_t e; //Copy relevant queens into local space
   e = async_work_group_copy(ws,&queens[group_size*q*2],2*group_size,0);
   wait_group_events(1,&e);
-  */
+  
 
   int i,j,conflicts = 0,curr[2];
   for(i = 0, j = 0; i<group_size; i++, j+=2){
@@ -36,10 +37,8 @@ __kernel void count_conflicts(const __global queen * queens,
     //curr[0] = queens[q*group_size*2+j];
     //curr[1] = queens[q*group_size*2+j+1];
 
-    //OpenCL spec says that the result of boolean expressions is 0 or 1
     conflicts += ((row == curr[0]) || (col == curr[1]) || 
 		  (abs_diff(row,curr[0]) == abs_diff(col,curr[1])));
-    //Occupied square
     conflicts += ((row == curr[0]) && (col == curr[1]))*nqueens;
   }
   
@@ -49,3 +48,89 @@ __kernel void count_conflicts(const __global queen * queens,
 
 }
 
+
+//Number of work-items should be a power of 2
+//Inspired by/borrowed from http://developer.amd.com/documentation/articles/Pages/OpenCL-Optimization-Case-Study-Simple-Reductions_3.aspx
+__kernel void reduce(__global queen * conflicts,
+		     const int nqueens,const int which,
+		     __local queen * scratch,
+		     __local queen * indexes,
+		     __global queen * result,
+		     __global int * result_i,
+		     __global int * cf_iters,
+		     const int col,const int rand){
+
+  int gid = get_global_id(0);
+  if(gid == 0){
+    if( (conflicts[col]) == nqueens+1){
+      cf_iters[0] = (cf_iters[0]+1);
+      conflicts[col] = -1;
+    }
+    else{
+      cf_iters[0] = 0;
+    }
+  }
+
+  barrier(CLK_GLOBAL_MEM_FENCE);
+
+  int lid = get_local_id(0);
+  int ind = gid < nqueens ? gid : -1;
+  if(gid < nqueens){
+      scratch[lid] = conflicts[gid];
+  }
+  else{
+    scratch[lid] = nqueens*nqueens;
+  }
+  indexes[lid] = ind;
+  barrier(CLK_LOCAL_MEM_FENCE);
+
+  int offset, local_size = get_local_size(0)/2;
+  for(offset = 1; offset > 0; offset >>=1 ){
+    if(lid < offset){
+      int other = lid + offset;
+      int mine = lid;
+      if(scratch[mine] < scratch[other] || 
+	 (rand && scratch[mine] == scratch[other])){
+	indexes[mine] = ind;
+      }
+      else{
+	scratch[mine] = scratch[other];
+	indexes[mine] = indexes[other];
+      }
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+  }
+  
+  if(lid == 0){
+    int groupid = get_group_id(0);
+    result[groupid] = scratch[0];
+    result_i[groupid] = indexes[0];
+  }
+}
+
+/*
+  Only 1 of these should ever be spawned at once. Changing 1 value in global
+  mem is probably faster than IO with host (read,change,write)
+ 
+  Looks at the reduced list of conflicts, selects the lowest 
+*/
+__kernel void make_move(const __global queen * conflicts,
+			const __global queen * indexes,
+			__global queen * queens,
+			const int which, const int nqueens,
+			const int size, const int rand){
+
+  int i,min_c=nqueens*nqueens,min_i;
+  for(i = 0; i < size && min_c > 1; i++){
+    if(conflicts[i] < min_c ||
+       (conflicts[i] == min_c && rand)){
+      min_c = conflicts[i];
+      min_i = indexes[i];
+    }
+  }
+
+  if(min_c != -1){
+    queens[2*which+1] = min_i;
+  }
+
+}
--- ir_parallel.cpp
+++ ir_parallel.cpp
@@ -9,9 +9,27 @@
 #define queen cl_int
 #endif
 
-#define BOARDSIZE 100
-#define LOCALSIZE 10
-#define MAX_ITERS 10000
+/*
+#define BOARDSIZE 8
+#define LOCALSIZE 1
+#define MAX_ITERS 200
+#define SEED 1336866913 //time(NULL)
+#define CHECK_ITERS 1
+
+#define REDUCE_LOCAL 2
+#define REDUCE_LEN BOARDSIZE/REDUCE_LOCAL
+*/
+
+#define BOARDSIZE 128
+#define LOCALSIZE 32
+#define MAX_ITERS 1000
+#define SEED 1336868812 //time(NULL)
+#define CHECK_ITERS 1
+
+#define REDUCE_LOCAL 2
+#define REDUCE_LEN BOARDSIZE/REDUCE_LOCAL
+
+
 
 using namespace std;
 
@@ -19,22 +37,28 @@ queen * random_board(); //Allocates space
 void print_board(queen * q);
 
 int main(){
-  srand(time(NULL));
+  int seed = SEED;
+  srand(seed);
+  printf("Seed: %u\n",seed);
 
   queen * queens = random_board(),* curr_q;
-  queen conflicts[BOARDSIZE] = {0};
+  queen conflicts[REDUCE_LEN],indexes[REDUCE_LEN],all_conflicts[BOARDSIZE];
   queen zero[BOARDSIZE] = {0};
   int curr = 0,cf_iters = 0,iters = 0,min_con,min_c;
-  int nqueens = BOARDSIZE,group_size=LOCALSIZE;
+  int nqueens = BOARDSIZE,group_size=LOCALSIZE,reduce_len=REDUCE_LEN;
+  int pseudo_rand;
   OpenCLWrapper w;
   //w.enableProfiling = true;
   
-  size_t globalWorkSize[1] = {BOARDSIZE*BOARDSIZE/LOCALSIZE};//queens,cols
-  size_t localWorkSize[1] = {BOARDSIZE};
+  size_t globalWorkSize[1] = {BOARDSIZE*BOARDSIZE/LOCALSIZE};
+  size_t localWorkSize[1] = {BOARDSIZE};  
 
-  //size_t globalWorkSize[1] = {BOARDSIZE};//queens,cols
-  //size_t localWorkSize[1] = {1};
+  size_t reduce_globalWorkSize[1] = {BOARDSIZE};//queens,cols
+  size_t reduce_localWorkSize[1] = {REDUCE_LOCAL};
+
+  size_t worksize_one[1] = {1};
   
+  bool done = false;
   try{
     
     w.createContext();
@@ -42,17 +66,36 @@ int main(){
     w.createProgram("ir.cl");
     
     w.createKernel("count_conflicts");
+    w.createKernel("reduce");
+    w.createKernel("make_move");
     
     //Queen array
     w.addMemObject(clCreateBuffer(w.context,
-				  CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+				  CL_MEM_USE_HOST_PTR | CL_MEM_READ_WRITE,
 				  sizeof(queen)*BOARDSIZE*2,
 				  queens,NULL));
     //Result array
     w.addMemObject(clCreateBuffer(w.context,
-				  CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
+				  CL_MEM_USE_HOST_PTR | CL_MEM_READ_WRITE,
 				  sizeof(queen)*BOARDSIZE,
-				  conflicts,NULL));
+				  zero,NULL));
+    
+    //Extra (result) arrays for reduce
+    w.addMemObject(clCreateBuffer(w.context,
+				  CL_MEM_READ_WRITE,
+				  sizeof(queen)*REDUCE_LEN,
+				  NULL,NULL));
+    w.addMemObject(clCreateBuffer(w.context,
+				  CL_MEM_USE_HOST_PTR | CL_MEM_READ_WRITE,
+				  sizeof(queen)*REDUCE_LEN,
+				  zero,NULL));
+
+    //cf_iter counter
+    w.addMemObject(clCreateBuffer(w.context,
+				  CL_MEM_READ_WRITE,
+				  sizeof(queen),
+				  NULL,NULL));
+    
     
     w.check(clSetKernelArg(w.kernels["count_conflicts"],0,sizeof(cl_mem),
 			   &w.memObjects[0]), "Error setting kernel arg 0");
@@ -61,50 +104,164 @@ int main(){
     w.check(clSetKernelArg(w.kernels["count_conflicts"],2,
 			   sizeof(queen)*LOCALSIZE*2,
 			   NULL), "Error setting kernel arg 2");
-
+    /*w.check(clSetKernelArg(w.kernels["count_conflicts"],3,sizeof(cl_int),
+      &nqueens), "Error setting kernel arg 3");*/
     w.check(clSetKernelArg(w.kernels["count_conflicts"],4,sizeof(cl_int),
 			   &nqueens), "Error setting kernel arg 4");
     w.check(clSetKernelArg(w.kernels["count_conflicts"],5,sizeof(cl_int),
 			   &group_size), "Error setting kernel arg 5");
-    
+
+
+    //Reduce kernel args
+    w.check(clSetKernelArg(w.kernels["reduce"],0,sizeof(cl_mem),
+			   &w.memObjects[1]), "Error setting kernel arg 0");
+    w.check(clSetKernelArg(w.kernels["reduce"],1,sizeof(cl_int),
+			   &nqueens), "Error setting kernel arg 1");
+    /*w.check(clSetKernelArg(w.kernels["reduce"],2,sizeof(cl_int),
+      &nqueens), "Error setting kernel arg 2");*/
+    w.check(clSetKernelArg(w.kernels["reduce"],3,
+			   sizeof(queen)*REDUCE_LOCAL,
+			   NULL), "Error setting kernel arg 3");
+    w.check(clSetKernelArg(w.kernels["reduce"],4,
+			   sizeof(queen)*REDUCE_LOCAL,
+			   NULL), "Error setting kernel arg 4");
+    w.check(clSetKernelArg(w.kernels["reduce"],5,sizeof(cl_mem),
+			   &w.memObjects[2]), "Error setting kernel arg 5");
+    w.check(clSetKernelArg(w.kernels["reduce"],6,sizeof(cl_mem),
+			   &w.memObjects[3]), "Error setting kernel arg 6");
+    w.check(clSetKernelArg(w.kernels["reduce"],7,sizeof(cl_mem),
+			   &w.memObjects[4]), "Error setting kernel arg 7");
+
+    //make_move args
+    w.check(clSetKernelArg(w.kernels["make_move"],0,sizeof(cl_mem),
+			   &w.memObjects[2]), "Error setting kernel arg 0");
+    w.check(clSetKernelArg(w.kernels["make_move"],1,sizeof(cl_mem),
+			   &w.memObjects[3]), "Error setting kernel arg 1");
+    w.check(clSetKernelArg(w.kernels["make_move"],2,sizeof(cl_mem),
+			   &w.memObjects[0]), "Error setting kernel arg 2");
+    /*w.check(clSetKernelArg(w.kernels["make_move"],3,sizeof(cl_mem),
+			   &w.memObjects[4]), "Error setting kernel arg 3");*/
+    w.check(clSetKernelArg(w.kernels["make_move"],4,sizeof(cl_int),
+			   &nqueens), "Error setting kernel arg 5");   
+    w.check(clSetKernelArg(w.kernels["make_move"],5,sizeof(cl_int),
+			   &reduce_len), "Error setting kernel arg 6");    
+
+    /*
+    printf("Board: %i\n",0);
+    print_board(queens);
+    */
+
     do{
+
       curr_q = &queens[2*curr];
 
       w.check(clSetKernelArg(w.kernels["count_conflicts"],3,sizeof(cl_int),
 			     &curr_q[0]), "Error setting kernel arg 3");
-      
-      w.check(
-	      clEnqueueWriteBuffer(w.commandQueue,w.memObjects[0],
-				   CL_FALSE,0,2*BOARDSIZE*sizeof(queen),
-				   queens,0,NULL,NULL),
-	      "Error enqueueing write buffer");
+      w.check(clSetKernelArg(w.kernels["reduce"],2,sizeof(cl_int),
+			     &curr_q[0]), "Error setting kernel arg 2");
+      w.check(clSetKernelArg(w.kernels["reduce"],8,sizeof(cl_int),
+			     &curr_q[1]), "Error setting kernel arg 8");
+      pseudo_rand = rand()%2;
+      w.check(clSetKernelArg(w.kernels["reduce"],9,sizeof(cl_int),
+			     &pseudo_rand), "Error setting kernel arg 9");
+
+      w.check(clSetKernelArg(w.kernels["make_move"],3,sizeof(cl_int),
+			     &curr), "Error setting kernel arg 4");
+      pseudo_rand = rand()%2;
+      w.check(clSetKernelArg(w.kernels["make_move"],6,sizeof(cl_int),
+			     &pseudo_rand), "Error setting kernel arg 4");
+
+
+      //printf("row: %i\n",curr_q[0]);
+      //Zero out conflicts count
+
       w.check(
 	      clEnqueueWriteBuffer(w.commandQueue,w.memObjects[1],
 				   CL_FALSE,0,BOARDSIZE*sizeof(queen),
 				   zero,0,NULL,NULL),
 	      "Error enqueueing write buffer");
 
-      
+      //Count conflicts
       w.check(
 	      clEnqueueNDRangeKernel(w.commandQueue,
 				     w.kernels["count_conflicts"],
 				     1,0,globalWorkSize,
 				     localWorkSize,0,NULL,NULL),
-	      "Error enqueueing kernel");
+	      "Error enqueueing count kernel");
+      //Reduce
       w.check(
-	      clEnqueueReadBuffer(w.commandQueue,w.memObjects[1],CL_TRUE,0,
-				  BOARDSIZE*sizeof(cl_int),conflicts,0,NULL,
+	      clEnqueueNDRangeKernel(w.commandQueue,
+				     w.kernels["reduce"],
+				     1,0,reduce_globalWorkSize,
+				     reduce_localWorkSize,0,NULL,NULL),
+	      "Error enqueueing reduce kernel");
+
+      //Make move
+      w.check(
+	      clEnqueueNDRangeKernel(w.commandQueue,
+				     w.kernels["make_move"],
+				     1,0,worksize_one,
+				     worksize_one,0,NULL,NULL),
+	      "Error enqueueing make move kernel");
+
+      //Occasionally check to see if we're done
+      if(iters%CHECK_ITERS == 0){
+	w.check(
+		clEnqueueReadBuffer(w.commandQueue,w.memObjects[4],CL_TRUE,0,
+				    sizeof(cl_int),&cf_iters,0,NULL,
+				    NULL),
+		"Error enqueueing read buffer");
+	//printf("cf_iters: %i\n",cf_iters);
+	if(cf_iters >= BOARDSIZE){
+	  w.check(
+		  clEnqueueReadBuffer(w.commandQueue,w.memObjects[0],CL_TRUE,0,
+				  BOARDSIZE*2*sizeof(cl_int),queens,0,NULL,
 				  NULL),
 		    "Error enqueueing read buffer");
-      
+	  done = true;
+	}
+      }
+
       /*
-      print_board(queens);
+      w.check(
+	      clEnqueueReadBuffer(w.commandQueue,w.memObjects[3],CL_TRUE,0,
+				  REDUCE_LEN*sizeof(cl_int),indexes,0,NULL,
+				  NULL),
+		    "Error enqueueing read buffer");
+
+      w.check(
+	      clEnqueueReadBuffer(w.commandQueue,w.memObjects[2],CL_TRUE,0,
+				  REDUCE_LEN*sizeof(cl_int),conflicts,0,NULL,
+				  NULL),
+		    "Error enqueueing read buffer");
+
+      w.check(
+	      clEnqueueReadBuffer(w.commandQueue,w.memObjects[1],CL_TRUE,0,
+				  BOARDSIZE*sizeof(cl_int),all_conflicts,0,
+				  NULL,NULL),
+		    "Error enqueueing read buffer");
+      */
+      w.check(
+	      clEnqueueReadBuffer(w.commandQueue,w.memObjects[0],CL_TRUE,0,
+				  BOARDSIZE*2*sizeof(cl_int),queens,0,
+				  NULL,NULL),
+	      "Error enqueueing read buffer");      
+
+      /*
       for(int i(0); i<BOARDSIZE; i++){
-	printf("%i,",conflicts[i]);
+	printf("%i,",all_conflicts[i]);
       }
       printf("\n");
+      for(int i(0); i<REDUCE_LEN; i++){
+	printf("Square (%i,%i) has %i conflicts\n",
+	       curr_q[0],indexes[i],conflicts[i]);
+      }
+      printf("\n");
+      printf("Board: %i\n",iters+1);
+      print_board(queens);
       */
 
+      /*
       if(conflicts[curr_q[1]]-BOARDSIZE > 1){
 	cf_iters = 0;
 	min_con = BOARDSIZE*BOARDSIZE;
@@ -118,11 +275,12 @@ int main(){
       }
       
       else{cf_iters++;}
+      */
 
       curr = (curr+1)%BOARDSIZE;
       iters++;
 
-    }while(cf_iters < BOARDSIZE && iters < MAX_ITERS);
+    }while(!done && iters < MAX_ITERS);
 
 
    }
@@ -132,7 +290,11 @@ int main(){
      return 1;    
    }   
 
-  printf("Iters: %i\n",iters);
+  if(done){
+    printf("Solved board of size %i in %i iterations.\n",BOARDSIZE,iters);
+    print_board(queens);
+  }
+
 
 }
 
